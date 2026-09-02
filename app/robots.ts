6a35108e8be04e80e78bb12066d1699f22751170
Orhan Polat <60147495+polatskiDev@users.noreply.github.com>
2026-09-02 09:46:03 +0200

    Update robots rules and sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,15 @@
 import { MetadataRoute } from 'next';
+import { siteConfig } from "@/lib/site-config";
+
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://zeybekacademy.nl/sitemap.xml',
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/admin",
+    },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
   };
 }
